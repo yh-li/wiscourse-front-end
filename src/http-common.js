f17b9de7f8857cf3a7a00350fd0dacca5ae05fc8
yihan.li@wisc.edu <yihan.li@wisc.edu>
2021-08-15 00:08:26 -0500

    edited proxy and server base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 //helper file
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/courses",
+  baseURL: "https://stormy-lake-18988.herokuapp.com/api/v1/courses",
   headers: {
     "Content-type": "application/json",
   },
