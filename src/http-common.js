611f58712f73e9df3bf8a54161bdd3f1e25f888a
yihan.li@wisc.edu <yihan.li@wisc.edu>
2021-08-27 01:04:28 -0500

    changed to heroku server

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 //helper file
 import axios from "axios";
 export default axios.create({
-  //baseURL: "https://stormy-lake-18988.herokuapp.com/api/v1/courses",
-  baseURL: "http://localhost:5000/api/v1/courses",
+  baseURL: "https://stormy-lake-18988.herokuapp.com/api/v1/courses",
+  //baseURL: "http://localhost:5000/api/v1/courses",
   headers: {
     "Content-type": "application/json",
   },
